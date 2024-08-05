8accd52da906a95554a920b2f385432a91c47f49
EmIlY452 <142310396+EmIlY452@users.noreply.github.com>
2024-08-05 08:12:56 -0300

    Add files via upload

--- Hardware/main.js
+++ Hardware/main.js
@@ -1,5 +1,4 @@
-
-const components = ['ram', 'cpu', 'gpu', 'keyboard', 'mouse', 'display','fonte','hd'];
+const components = ['ram', 'cpu', 'gpu', 'keyboard', 'mouse', 'display', 'fonte', 'hd'];
 
 components.forEach(component => {
   const compElem = document.getElementById(component);
@@ -10,30 +9,28 @@ components.forEach(component => {
   compElem.addEventListener('dragstart', (e) => {
     e.dataTransfer.setData('text', e.target.id);
     if (component === 'ram') {
-      compElem.style.transform = 'rotate(90deg)'; // Girar ao iniciar o arrasto
+      compElem.style.transform = 'rotate(90deg)';
     }
   });
 
   // Adicionar evento para atualizar posição durante arrasto
   document.addEventListener('dragover', (e) => {
+    e.preventDefault();
     const data = e.dataTransfer.getData('text');
-    if (data === component) {
-      e.preventDefault();
-      if (component === 'ram') {
-        const offsetX = e.clientX - compElem.clientWidth / 2;
-        const offsetY = e.clientY - compElem.clientHeight / 2;
-        compElem.style.position = 'absolute';
-        compElem.style.left = `${offsetX}px`;
-        compElem.style.top = `${offsetY}px`;
-      }
+    if (data === compElem.id && component === 'ram') {
+      const offsetX = e.clientX - compElem.clientHeight / 2;
+      const offsetY = e.clientY - compElem.clientWidth / 2;
+      compElem.style.position = 'absolute';
+      compElem.style.left = `${offsetX}px`;
+      compElem.style.top = `${offsetY}px`;
     }
   });
 
   compElem.addEventListener('dragend', (e) => {
     if (component === 'ram') {
-      compElem.style.transform = ''; // Voltar à rotação original se não for solto no slot
-      compElem.style.position = ''; // Resetar a posição após arrastar
+      compElem.style.transform = ''; // Remover rotação após arrasto
     }
+    compElem.style.position = ''; // Resetar a posição após arrastar
   });
 
   // Eventos para todos os componentes
@@ -51,13 +48,14 @@ components.forEach(component => {
       slotElem.style.border = '2px dashed green'; // Indicação de sucesso
     } else {
       alert('Erro: Imagem incorreta!');
-      slotElem.style.border = '2px dashed red'; // Indicação de erro
+      slotElem.style.border = '4px dashed red'; // Indicação de erro
       if (component === 'ram') {
-        compElem.style.transform = ''; // Voltar à rotação original em caso de erro
+        compElem.style.transform = ''; // Remover rotação em caso de erro
       }
     }
   });
 });
+
 function exibirResolucaoTela() {
   var largura = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   var altura = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
