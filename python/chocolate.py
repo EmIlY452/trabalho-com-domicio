c70469aa6e8a79c92ed1fb5dd418c9206bb5c98a
EmIlY452 <142310396+EmIlY452@users.noreply.github.com>
2025-02-14 11:36:09 -0300

    Add files via upload

---
+++ python/chocolate.py
@@ -0,0 +1,19 @@
+valor1 =float(input("Digite o primeiro valor R$:"))
+valor2 =float(input("Digite o segundo valor R$"))
+
+
+resultado = valor1 + valor2
+
+print("a soma dos dois valores é R$", resultado)
+
+
+
+
+
+
+
+
+
+
+
+
